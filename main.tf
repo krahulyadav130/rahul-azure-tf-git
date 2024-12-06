e15bc1a284f42c737bbfe2b4a765cc11a7daf0ed
rahul <k.rahulyadav130@outlook.com>
2024-12-06 11:22:22 +0530

    second

--- main.tf
+++ main.tf
@@ -1,5 +1,36 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "<= 3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">=2.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "0.9.1"
+    }
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>1.5"
+    }
+  }
+  # backend "azurerm" {
+  #   container_name = "rahultest-tfstate"
+  #   storage_account_name = "terraformprojecttfmanual"
+  #   key = "rahul-test-dev"
+  #   resource_group_name = "myrsg-rahul-portal"
+    
+  # }
+}
+
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
+   
   features {}
+  
 }
 
 resource "azurerm_resource_group" "example" {
